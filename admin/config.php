712bbd8899ce3744170ab8de3ea56b3fa20a1bd3
Radoslav Andonov <Radoslav.Andonov@softwareag.com>
2024-05-10 15:18:23 +0300

    Initial Commit

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'https://test.photofamily.bg/admin/');
+define('HTTP_CATALOG', 'https://test.photofamily.bg/');
+
+// HTTPS
+define('HTTPS_SERVER', 'https://test.photofamily.bg/admin/');
+define('HTTPS_CATALOG', 'https://test.photofamily.bg/');
+
+// DIR
+define('DIR_APPLICATION', '/home/photofam/test.photofamily.bg/admin/');
+define('DIR_SYSTEM', '/home/photofam/test.photofamily.bg/system/');
+define('DIR_IMAGE', '/home/photofam/test.photofamily.bg/image/');
+define('DIR_LANGUAGE', '/home/photofam/test.photofamily.bg/admin/language/');
+define('DIR_TEMPLATE', '/home/photofam/test.photofamily.bg/admin/view/template/');
+define('DIR_CONFIG', '/home/photofam/test.photofamily.bg/system/config/');
+define('DIR_CACHE', '/home/photofam/test.photofamily.bg/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/photofam/test.photofamily.bg/system/storage/download/');
+define('DIR_LOGS', '/home/photofam/test.photofamily.bg/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/photofam/test.photofamily.bg/system/storage/modification/');
+define('DIR_UPLOAD', '/home/photofam/test.photofamily.bg/system/storage/upload/');
+define('DIR_CATALOG', '/home/photofam/test.photofamily.bg/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'photofam_radko');
+define('DB_PASSWORD', 'R@dko123');
+define('DB_DATABASE', 'photofam_adelina_handmade');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'https://test.photofamily.bg/');
+
+// HTTPS
+define('HTTPS_SERVER', 'https://test.photofamily.bg/');
+
+// DIR
+define('DIR_APPLICATION', '/home/photofam/test.photofamily.bg/catalog/');
+define('DIR_SYSTEM', '/home/photofam/test.photofamily.bg/system/');
+define('DIR_IMAGE', '/home/photofam/test.photofamily.bg/image/');
+define('DIR_LANGUAGE', '/home/photofam/test.photofamily.bg/catalog/language/');
+define('DIR_TEMPLATE', '/home/photofam/test.photofamily.bg/catalog/view/theme/');
+define('DIR_CONFIG', '/home/photofam/test.photofamily.bg/system/config/');
+define('DIR_CACHE', '/home/photofam/test.photofamily.bg/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/photofam/test.photofamily.bg/system/storage/download/');
+define('DIR_LOGS', '/home/photofam/test.photofamily.bg/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/photofam/test.photofamily.bg/system/storage/modification/');
+define('DIR_UPLOAD', '/home/photofam/test.photofamily.bg/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'photofam_radko');
+define('DB_PASSWORD', 'R@dko123');
+define('DB_DATABASE', 'photofam_adelina_handmade');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
